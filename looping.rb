c2e089d3392453cce60dd01fef8d422d9f049251
Neil Cochrane <neilc@cns-gc.com.au>
2022-12-06 05:42:19 +1000

    NC Completed Ruby Control Flow Loops lab

--- looping.rb
+++ looping.rb
@@ -1,5 +1,11 @@
 def happy_new_year
   # your code here
+  i = 10
+  while i > 0
+    puts i
+    i -= 1
+  end
+  puts "Happy New Year!"
 end
 
 # No need to modify this code! Use this to implement the fizzbuzz_printer method.
@@ -17,8 +23,18 @@ end
 
 def fizzbuzz_printer
   # your code here
+  i = 1
+  while i < 101
+    puts fizzbuzz(i)
+    i += 1
+  end
 end
 
 def reverse_string(str)
   # your code here
+  my_string = ""
+  str.length.times do |i|
+    my_string = str[i] + my_string
+  end
+  my_string
 end
